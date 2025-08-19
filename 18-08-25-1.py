54e01ffc8f0c271bfb08c5a2d68971f5b030d85c
Beri-a01821381 <a01821381@tec.mx>
2025-08-19 07:50:54 -0600

    Add files via upload

---
+++ 18-08-25-1.py
@@ -0,0 +1,11 @@
+print("Area de un triangulo")
+# Entrada b y h
+# Proceso (b*h)/2
+# Salida a
+b=float(input("Indique la base de su triangulo (No puede ser igual a 0): "))
+h=float(input("Indique la altura de su triangulo (No puede ser igual a 0): "))
+#if b or h : 0
+#print ("Elija otro valor ")
+a=(b*h)/2
+print("El area de su triangulo = ",a)
+
---
+++ 18-08-25-2.py
@@ -0,0 +1,14 @@
+print("Promedio de 4 calificaciones")
+#Entrada calf1, calf2, calf3, calf4, prom
+#Proceso (calf1+calf2+calf3+calf4)/4
+#Salida prom
+
+#Entrada
+calf1=(int(input("Indique la calificación 1: ")))
+calf2=(int(input("Indique la calificación 2: ")))
+calf3=(int(input("Indique la calificación 3: ")))
+calf4=(int(input("Indique la calificación 4: ")))
+#Proceso
+prom=(calf1+calf2+calf3+calf4)/4
+#Salida
+print(f"Su promedio de las 4 calificaciones: {prom:.2f}")
