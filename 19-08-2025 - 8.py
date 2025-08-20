828ead4cd88f4584533fcbfc4ff03a9f1524ce14
Beri-a01821381 <a01821381@tec.mx>
2025-08-19 23:50:53 -0600

    Add files via upload

---
+++ 19-08-2025 - 8.py
@@ -0,0 +1,11 @@
+print("Distancia recorrida por un caracol")
+#Entrada minutos, distancia
+#Proceso (minutos*60)/100
+#Salida total
+
+#Entrada
+minutos=float(input("Indique cuantos minutos vio recorrer el caracol: "))
+#Proceso 
+distancia=((minutos*60)*5.7)/100
+#Salida
+print(f"La distancia total recorrida fue = {distancia:.2f}")
